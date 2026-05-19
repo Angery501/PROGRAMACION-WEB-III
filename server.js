8e89b22953cd1d38c0439aa6f35236d1b6ac2b3c
Angery501 <aracely.naomi.prado@gmail.com>
2026-05-18 18:53:00 -0700

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-const categoriasRoutes = require('./routes/categorias');
-app.use('/categorias', categoriasRoutes);
-app.listen(3000, () => {
-    console.log('Servidor corriendo en puerto 3000');
-});
